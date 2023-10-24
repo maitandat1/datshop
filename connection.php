fe9b8ae55882d47ca606cfcffb0bac616f93effc
maitandat1 <145008872+maitandat1@users.noreply.github.com>
2023-10-24 10:16:56 +0700

    neee

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php 
-    $conn = mysqli_connect('grp6m5lz95d9exiz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','ndvikidfsr3888yv','wp5mff3afn6wq3sc','uy0p9mzut7qyaq2a') 
+    $conn = mysqli_connect('grp6m5lz95d9exiz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','yp6fskp6rksz564u','hkn81fhwt177oh0m','cxx5mxypguqfctso') 
     or die("Cannot connect database".mysqli_connect_error());
 
     // $conn = mysqli_connect('vkh7buea61avxg07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','mfabx3yyukb47dhz','my4f1ao60ko7khl5','pnhrde2ut64ol8sa') 
